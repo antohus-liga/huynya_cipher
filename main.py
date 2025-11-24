04899e0931abe52b6d96fc80f120fb24f6498fc4
antohus-liga <antonioslotylo0904@gmail.com>
2025-11-24 08:02:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# ᚠᚡᚢᚣᚤᚥᚦᚧᚨᚩᚪᚫᚬᚭᚮᚯᚰᚱᚲᚳᚴᚵᚶᚷᚸᚹ ᛫᛬᛭ᛮᛯᛰ
+# ☉☿♁♃♄♅♆♇⚳⚴⚵⚶⚷⚸⚹⚺⚻⚼⌖⌬⍟⍤⍥⍨⍩⍭ ∴∵⍪⸮‼-
+
+"""
+make options 1 - encode; 2 - decode.
+
+first make a string that has all characters needed
+then make a dictionary that has `cipher keys` as `dict keys`
+and `encoded characters` as values (see the two examples above)
+in a `for` loop, when DECODING, get the index of the current character
+from the value of the current key character, return the character that has this
+index in the human alphabet
+when ENCODING, get the index of the current character in the human alphabet
+then return the `encoded character` from the value of the current key character
+
+after testing add error handling so it's more convenient to use
+"""
